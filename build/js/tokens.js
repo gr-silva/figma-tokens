b3f565a43554062de13affbf3caf401befc5abf1
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-05-24 00:55:23 -0300

    feat: initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 24 May 2024 03:52:02 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
