a47f7e502c7103cb11322900faf8313b0afb10fc
gr-silva <gr-silva@users.noreply.github.com>
2024-05-24 04:23:50 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Fri, 24 May 2024 03:52:02 GMT
+ * Generated on Fri, 24 May 2024 04:23:49 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214",
+  "ColorPurple400": "#9874f3",
   "ColorPurple500": "#8257e6"
 };
